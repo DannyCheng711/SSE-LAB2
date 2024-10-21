0dc2f44fdb9964d238564cc15ef6a199602fcc37
cc2224 <chieh-tung.cheng24@imperial.ac.uk>
2024-10-21 15:11:47 +0100

    feat:add_pytest_yml

--- src/app.py
+++ src/app.py
@@ -14,3 +14,19 @@ def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
     return render_template("hello.html", name=input_name, age=input_age)
+
+def process_query(q):
+    if q == "dinosaurs":
+        return "Dinosaurs ruled the Earth 200 million years ago"
+    else:
+        return "Unknown"
+
+# get the dinosaurs
+@app.route("/query", methods=["GET"])
+def query():
+    q = request.args.get('q')
+    response = process_query(q)
+    
+    return response
+
+    
